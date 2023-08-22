d4538deb9b71ee66202bf6ad4c98c0ddd0aef54a
cristianvazquezr <vazquezcristianr@gmail.com>
2023-08-22 14:44:07 -0300

    correccion de error

--- src/Routes/views.router.js
+++ src/Routes/views.router.js
@@ -24,7 +24,7 @@ viewsRouter.get('/', async (req,resp)=>{
         
         const newProductos=productos.map(elemento=>elemento)
         newProductos.splice(limit-1,productos.length-limit)
-        listProduct = newProductos
+        const listProduct = newProductos
         resp.render("home",{
             product:listProduct,
             style:"style.css"
@@ -33,27 +33,9 @@ viewsRouter.get('/', async (req,resp)=>{
 })
 
 viewsRouter.get('/realtimeproducts', async (req,resp)=>{
-    let limit=req.query.limit
-    //con un condicional veo si existe query y que sea numero.
-
-    let productos=await PM.getProducts()
-   
-    if((limit==undefined || isNaN(limit) || limit>productos.length )){
-        const listProduct = await PM.getProducts()
-        resp.render("realTimeProducts",{
-            product:listProduct,
-            style:"style.css"
-        })
-    }else{
-        
-        const newProductos=productos.map(elemento=>elemento)
-        newProductos.splice(limit-1,productos.length-limit)
-        listProduct = newProductos
-        resp.render("realTimeProducts",{
-            product:listProduct,
-            style:"style.css"
-        })
-    } 
+    resp.render("realTimeProducts",{
+        style:"style.css"
+    })
 })
 
-export default viewsRouter
+export default viewsRouter  
